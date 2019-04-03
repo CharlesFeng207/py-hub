5d079087d90d2af1dab22e1be206bd3b28b1eb03
DESKTOP-PRCLPLD\47613 <CharlesFeng207@gmail.com>
2019-04-03 20:40:56 +0800

    123

--- test/debugconsole_server/debugconsole_server.py
+++ test/debugconsole_server/debugconsole_server.py
@@ -1,7 +1,9 @@
 # coding=utf-8
 import socket
 import hashlib
-
+import json
+import os
+import traceback
 
 if __name__ == "__main__":
     secret = "xEdyaR+b0BE4yK3dy5yC0JDe7b8GV2lJ5dHl05cChFVEcSl2LI+EkJEKNblXK3Mi"
@@ -10,8 +12,32 @@ if __name__ == "__main__":
     print("bind udp on port:32979 ...")
 
     while True:
-        data, addr = s.recvfrom(1024)
-        info = data.decode(encoding="utf-8")
-        print("Receive from {} {}".format(info, addr))
-        signature = hashlib.md5((info + secret).encode('utf8')).hexdigest()
-        s.sendto(signature.encode('utf8'), addr)
+        try:
+            data, addr = s.recvfrom(10240)
+            info = data.decode(encoding="utf-8")
+            msg = json.loads(info)
+
+            if msg["loginStr"] is not None:
+                print("{} from {}".format(info, addr))
+                signature = hashlib.md5((msg["loginStr"] + secret).encode('utf8')).hexdigest()
+                s.sendto(signature.encode('utf8'), addr)
+
+            if msg["log"] is not None:
+                folder = os.path.join("debugconsole_logs", msg["appName"], msg["deviceInfo"])
+                if not os.path.exists(folder):
+                    os.makedirs(folder)
+
+                p = os.path.join(folder, msg["launchTime"] + '.log')
+
+                if not os.path.exists(p):
+                    print("create " + p)
+
+                f = open(p, 'a+')
+                f.write(msg["log"] + "\n")
+                f.close()
+        except Exception as e:
+            print(e)
+            print(traceback.format_exc())
+            pass
+
+

baca46e06cc7a13484795534b7aa833c45dc9d31
ALIENWARE-CHARL\Charles <CharlesFeng207@gmail.com>
2019-08-26 23:03:27 +0800

    z

--- test/basic.py
+++ test/basic.py
@@ -17,6 +17,8 @@ s = '漢  \n χαν  хан \ud83d\ude2c\ud83d\ude2c'
 print('解回字符串: ' + s.encode("utf-8", "ignore").decode('utf-8'))
 
 print(b"\351\273\204\351\207\221".decode("utf-8"))
+print(b"\xe9\xbb\x84\xe9\x87\x91".decode("utf-8")) #https://www.bejson.com/convert/ox2str/
+
 
 with open('test.txt', "w+", encoding="utf-8", errors="ignore") as out_file:
     out_file.write(s)

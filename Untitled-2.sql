ec14f7a10ea7f4250fbb16ddac6b0c32a7463892
DESKTOP-PRCLPLD\47613 <CharlesFeng207@gmail.com>
2019-09-04 18:24:34 +0800

    1

---
+++ Untitled-2.sql
@@ -0,0 +1,12 @@
+/* SELECT max(lanId) FROM `language`; */
+/* show table status like "language"; */
+/* delete from language where lanId=12427; */
+/* ALTER TABLE language MODIFY lanId INT NOT NULL AUTO_INCREMENT; */
+/* INSERT INTO language (cn) VALUES ("你妹"); */
+/* ALTER TABLE language Auto_increment=12426; */
+/* SELECT * FROM `language`.`language` where lanId=0; */
+/* SELECT * FROM `language` where lanId>12000; */
+/* SELECT * FROM `language` where cn="限 时"; */
+/* INSERT INTO language ( lanId, cn, en, zh, jp, ko ) VALUES (0, "", "", "", "", ""); */
+/* select count(*) from language; */
+/* select max(lanId) from language; */

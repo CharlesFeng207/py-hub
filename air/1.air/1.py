520958636c082fb1c4dd971fe317abb3e9cac2f7
ALIENWARE-CHARL\Charles <CharlesFeng207@gmail.com>
2020-02-09 17:16:17 +0800

    123

---
+++ air/1.air/1.py
@@ -0,0 +1,10 @@
+# -*- encoding=utf8 -*-
+__author__ = "Charles"
+
+from airtest.core.api import *
+
+while True:
+    touch(Template(r"tpl1581177203083.png", record_pos=(-0.115, 0.024), resolution=(1806, 864)))
+    touch(Template(r"tpl1581177431271.png", record_pos=(-0.148, -0.021), resolution=(1806, 864)))
+
+    sleep(2)
---
+++ air/1.air/2.py
@@ -0,0 +1,4 @@
+
+from pywinauto.application import Application
+
+print("1")

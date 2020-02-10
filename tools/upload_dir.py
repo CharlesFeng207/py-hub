534429ece2cfb3f981b254b7a750437b94e576ef
ALIENWARE-CHARL\Charles <CharlesFeng207@gmail.com>
2020-02-10 10:39:09 +0800

    1

--- tools/upload_dir.py
+++ tools/upload_dir.py
@@ -4,6 +4,35 @@ import json
 import os
 import pysftp
 import sys
+from time import sleep
+
+
+def run():
+    print("run...")
+    srv = None
+    os.chdir(localdir)
+    files = os.listdir(localdir)
+    for i, filename in enumerate(files):
+        if filename.endswith(".part"):
+            continue
+
+        if not srv:
+            cnopts = pysftp.CnOpts()
+            cnopts.hostkeys = None
+            srv = pysftp.Connection(host=config["host"], port=config["port"], username=config["username"],
+                                    password=config["password"], cnopts=cnopts)
+
+        remotepath = config["remotedir"] + "/" + filename
+        print("process {}, {} / {}".format(filename, i+1, len(files)))
+        print(f"{filename} -> {remotepath} ...")
+        srv.put(filename, remotepath, callback=printProgressDecimal)
+        os.remove(filename)
+        print(f"{filename} deleted!")
+
+
+if not srv:
+    srv.close()
+    srv = None
 
 progressDict = {}
 progressEveryPercent = 10
@@ -29,21 +58,11 @@ localdir = config["localdir"]
 if not os.path.isdir(localdir):
     raise Exception(f"{localdir} is not dir!")
 
-cnopts = pysftp.CnOpts()
-cnopts.hostkeys = None
-srv = pysftp.Connection(host=config["host"], port=config["port"], username=config["username"],
-                        password=config["password"], cnopts=cnopts)
-
-os.chdir(localdir)
-files = os.listdir(localdir)
-for i, filename in enumerate(files):
-    if filename.endswith(".part"):
-        continue
-    remotepath = config["remotedir"] + "/" + filename
-    print("process {}, {} / {}".format(filename, i+1, len(files)))
-    print(f"{filename} -> {remotepath} ...")
-    srv.put(filename, remotepath, callback=printProgressDecimal)
-    os.remove(filename)
-    print(f"{filename} deleted!")
-
-srv.close()
+if len(sys.argv) > 1 and sys.argv[1] == "-t":
+    while True:
+        run()
+        sleep(5)
+        pass
+else:
+    run()
+    pass
